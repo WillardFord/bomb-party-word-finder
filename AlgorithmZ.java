465d9301ab5f1b46437ecb8bbf8eeb2ccd033910
Willard Washington Ford <willardwashingtonford@gmail.com>
2023-01-19 13:39:03 -0800

    Added functionality that makes AlgorithmZ convenient to use with bomb party.

--- AlgorithmZ.java
+++ AlgorithmZ.java
@@ -1,19 +1,22 @@
 // 1.4 Fundamental Pre-processing in Linear Time
 
 import java.util.Scanner;
+import java.io.*;
 
 public class AlgorithmZ {
     int[] Zscores;
     Integer l, r;
     String processing_str;
     Integer find_str_length;
+    String search_str;
 
-    AlgorithmZ(){
+    AlgorithmZ(String search_str){
         this.processing_str = "";
         this.find_str_length = 0;
         this.l = 0;
         this.r = 0;
         this.Zscores = null;
+        this.search_str = search_str;
     }
     /**
      * This function takes a search_str and find_str concats them together into a 
@@ -21,11 +24,11 @@ public class AlgorithmZ {
      * Then it generates the Z scores stored in an array Zscores using fundamental 
      * preprocessing of strings algorithm.
      */
-    void generate_Zscores(String find_str, String search_str){
+    void generate_Zscores(String find_str){
         this.l = 0;
         this.r = 0;
         this.find_str_length = find_str.length();
-        this.processing_str = find_str + "$" + search_str;
+        this.processing_str = find_str + "$" + this.search_str;
         Integer processing_str_length = processing_str.length();
         this.Zscores = new int[processing_str_length];
 
@@ -74,23 +77,50 @@ public class AlgorithmZ {
     void find(){
         for(int index = 1; index < this.Zscores.length ; index ++){
             if(Zscores[index] == this.find_str_length){
-                System.out.println(index - find_str_length - 1);
+                int left_index = index - find_str_length - 1;
+                while(search_str.charAt(left_index) != '#') left_index --;
+                int right_index = index - find_str_length - 1;
+                while(search_str.charAt(right_index) != '#') right_index ++;
+                
+                System.out.println(this.search_str.substring(left_index + 1 , right_index));
             }
         }
     }
 
     public static void main(String[] args){
-        AlgorithmZ string_searcher = new AlgorithmZ();
+        // Add dictionary to search str, separate each character by a #.
+        String search_str = "";
 
-        Scanner user_input = new Scanner(System.in);
-        System.out.println("Enter string to be searched:");
-        String search_str = user_input.nextLine();
-        System.out.println("Enter string to find:");
-        String find_str = user_input.nextLine();
-        user_input.close();
+        try{
+            FileInputStream fstream = new FileInputStream("src/dict.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            while ((strLine = br.readLine()) != null)   {
+                search_str += strLine.toLowerCase() + "#";
+            }
+            in.close();
+        }
+        catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
 
-        string_searcher.generate_Zscores(find_str, search_str);
-        string_searcher.find();
+        //System.out.println("Enter string to be searched:");
+        //String search_str = user_input.nextLine();
+        AlgorithmZ string_searcher = new AlgorithmZ(search_str);
 
+        try{
+            Scanner user_input = new Scanner(System.in);
+            while(true){
+                
+                System.out.println("Enter string to find:");
+                String find_str = user_input.nextLine();
+                
+                string_searcher.generate_Zscores(find_str);
+                string_searcher.find();
+            }
+        }catch(Exception E){
+            System.out.println("System.in was closed; exiting");
+        }
     }
 }
