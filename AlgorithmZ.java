c03e2cbc7dad812f5b541c552454ff2f1cb0638a
Willard Washington Ford <willardwashingtonford@gmail.com>
2024-07-07 21:49:38 -0400

    Made more usable
    Only returns one word now. Sorts by shortest words so they are easier to type. Pressing enter without submitting a new string will return the next word for the previous prompt in cases where the word is locked.

--- AlgorithmZ.java
+++ AlgorithmZ.java
@@ -1,7 +1,15 @@
 // 1.4 Fundamental Pre-processing in Linear Time
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.io.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 
 public class AlgorithmZ {
     int[] Zscores;
@@ -44,7 +52,7 @@ public class AlgorithmZ {
                     this.r = index + start - 1;
                     this.l = index;
                 }
-            } 
+            }
             else{
                 int z_prime = Zscores[index - l];
                 if( z_prime < r - index + 1){
@@ -74,17 +82,19 @@ public class AlgorithmZ {
      * This function simply iterates through the Zscores and prints the indices in the 
      * search_str that corresponds to the found find_str.
      */
-    void find(){
-        for(int index = 1; index < this.Zscores.length ; index += 100){
+    ArrayList<String> find(){
+        ArrayList<String> output_words = new ArrayList<String>();
+        for(int index = 1; index < this.Zscores.length ; index += 1){
             if(Zscores[index] == this.find_str_length){
                 int left_index = index - find_str_length - 1;
                 while(search_str.charAt(left_index) != '#') left_index --;
                 int right_index = index - find_str_length - 1;
                 while(search_str.charAt(right_index) != '#') right_index ++;
                 
-                System.out.println(this.search_str.substring(left_index + 1 , right_index));
+                output_words.add(this.search_str.substring(left_index + 1 , right_index));
             }
         }
+        return output_words;
     }
 
     public static void main(String[] args){
@@ -110,14 +120,69 @@ public class AlgorithmZ {
         AlgorithmZ string_searcher = new AlgorithmZ(search_str);
 
         try{
+            String newline = System.getProperty("line.separator");
             Scanner user_input = new Scanner(System.in);
+            HashSet<String> seen_words = new HashSet<String>();
+            HashSet<Character> seen_chars = new HashSet<Character>();
+
+            ArrayList<String> possible_words = new ArrayList<String>();
+            int j = 0;
             while(true){
                 
                 System.out.println("Enter string to find:");
                 String find_str = user_input.nextLine();
-                
+
+                if (find_str == ""){
+                    j++;
+                    String new_word = possible_words.get(j);
+                    seen_words.add(new_word);
+                    for (Character letter: new_word.toCharArray()){
+                        seen_chars.add(letter);
+                    }
+
+                    if (seen_chars.size() > 25) {
+                        seen_chars = new HashSet<Character>();
+                    }
+                    System.out.println(new_word);
+                    continue;
+                }
+                j = 0;
+                possible_words = new ArrayList<String>();
                 string_searcher.generate_Zscores(find_str);
-                string_searcher.find();
+
+
+                
+            
+                ArrayList<String> found_words = string_searcher.find();
+                Collections.sort(found_words, Comparator.comparingInt(String::length));
+                for (String word: found_words) {
+                    if (!seen_words.contains(word)) {
+                        for (Character letter: word.toCharArray()){
+                            if (!seen_chars.contains(letter)){
+                                possible_words.add(word);
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (possible_words.size() < 1) {
+                    for (int i = 0; i < 5; i++){
+                        possible_words = found_words;
+                    }
+                }
+                String the_word = possible_words.get(j);
+                System.out.println(the_word);
+
+                seen_words.add(the_word);
+                for (Character letter: the_word.toCharArray()){
+                    seen_chars.add(letter);
+                }
+
+                if (seen_chars.size() > 25) {
+                    seen_chars = new HashSet<Character>();
+                }
+
+
             }
         }catch(Exception E){
             System.out.println("System.in was closed; exiting");
